44149182cc97b58af61f7144001a944d8e2b6309
Rafid-Al-Mahmud <76583107+Rafid-Al-Mahmud@users.noreply.github.com>
2022-12-14 03:12:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get(/.*/, (req, res) => {
-  res.send('Hello World!')
+  res.send(`
+            <script>
+            const xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                var data = JSON.parse(this.responseText);
+                // document.querySelector("body").innerHTML +=
+                // i+": "+data.bg+"<br>";
+                document.body.style.background=data.bg;
+                i++;
+            }
+            xhttp.open("GET", 'http://aws.random.cat/meow');
+            xhttp.send();
+            </script>
+  `);
 })
 
 app.listen(port, () => {
