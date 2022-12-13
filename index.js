11a3abe65f8a40dd57774b0c1704f6d0cc3a0b4d
Rafid-Al-Mahmud <76583107+Rafid-Al-Mahmud@users.noreply.github.com>
2022-12-14 03:26:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get(/.*/, (req, res) => {
-  res.send(`
-            <script>
-            const xhttp = new XMLHttpRequest();
-            xhttp.onload = function() {
-                var data = JSON.parse(this.responseText);
-                document.querySelector("body").innerHTML = "<img src="${data.file}";
-            }
-            xhttp.open("GET", 'http://aws.random.cat/meow');
-            xhttp.send();
-            </script>
-  `);
+    res.send(`
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+    <script>
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        var data = JSON.parse(this.responseText);
+        document.querySelector("body").innerHTML = "<img src="${data.file}";
+    }
+    xhttp.open("GET", 'http://aws.random.cat/meow');
+    xhttp.send();
+    </script>
+</body>
+</html>
+    `);
 })
 
 app.listen(port, () => {
